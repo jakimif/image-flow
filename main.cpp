fec8255e17eb9b49442cf3de675f4be015989601
André França <avila.andre31@gmail.com>
2019-05-01 10:00:00 -0300

    validate the folder (folder) and filter (filter) passed by the command line

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include "argumentParser.h"
 
 #include <array>
+#include <filesystem>
 #include <iomanip>
 #include <iostream>
 
 namespace Args {
 namespace Flags {
-static const char* Rename = "rename";
-static const char* Convert = "convert";
-static const char* Resize = "resize";
-static const char* Scale = "scale";
+static constexpr const char* Rename = "rename";
+static constexpr const char* Convert = "convert";
+static constexpr const char* Resize = "resize";
+static constexpr const char* Scale = "scale";
 } // namespace Flags
+namespace Opts {
+static constexpr const char* Folder = "folder";
+static constexpr const char* Filter = "filter";
+static constexpr const char* Amount = "amount";
+} // namespace Opts
 } // namespace Args
 
 void validateArguments(const ArgumentParser& argParser) {
@@ -30,7 +36,7 @@ void validateArguments(const ArgumentParser& argParser) {
 	std::cout << std::boolalpha << "Scale: " << argParser.getFlag(Args::Flags::Scale) << "\n";
 
 	if (numActiveModes != 1) {
-		throw std::invalid_argument("Somente um modo pode estar ativo.");
+		throw std::invalid_argument("only one mode can be active.");
 	}
 
 	// verificar se existem apenas 1 modo, utilizando OR Exclusivo
@@ -43,6 +49,25 @@ void validateArguments(const ArgumentParser& argParser) {
 	// if (!(bRenameMode ^ bConvertMode ^ bResizeMode ^ bScaleMode)) {
 	// 	throw std::invalid_argument("Somente um modo pode estar ativo.");
 	// }
+
+	// validar a pasta passada como a opção folder
+	const std::string folder = argParser.getOptionAs<std::string>(Args::Opts::Folder);
+	if (folder.empty()) {
+		throw std::invalid_argument("the folder cannot be blank.");
+	}
+
+	if (!std::filesystem::exists(folder)) {
+		throw std::invalid_argument("the folder does not exist.");
+	}
+
+	// validar se o filtro é uma string valida
+	const std::string filter = argParser.getOptionAs<std::string>(Args::Opts::Filter);
+	if (!filter.empty()) {
+		std::string invalidCaracteres = "\\/*?\"<>|"; // \/*?"<>|
+		if (filter.find_first_of(invalidCaracteres) != std::string::npos) {
+			throw std::invalid_argument("the filter cannot contain: " + invalidCaracteres);
+		}
+	}
 }
 
 int main(int argc, char* argv[]) {
@@ -53,14 +78,10 @@ int main(int argc, char* argv[]) {
 	argParser.registerFlag(Args::Flags::Resize);
 	argParser.registerFlag(Args::Flags::Scale);
 
-	argParser.registerOption("folder");
-	argParser.registerOption("amount");
+	argParser.registerOption(Args::Opts::Folder);
+	argParser.registerOption(Args::Opts::Filter);
 
 	argParser.parse(argc, argv);
-
-	// std::cout << "Folder: " << argParser.getOptionAs<std::string>("folder") << "\n";
-	// std::cout << "Amount: " << argParser.getOptionAs<float>("amount") << "\n";
-	// std::cout << "Amount: " << argParser.getOptionAs<int>("amount") << "\n";
 
 	try {
 		validateArguments(argParser);
